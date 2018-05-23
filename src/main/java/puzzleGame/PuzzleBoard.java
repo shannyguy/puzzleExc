fc34a2c3d9d52f8625191dcdc4bbd1ad011ac4e4
Lior Nachmias <ln699k@att.com>
2018-05-23 23:46:05 +0300

    Adding puzzle validator utility including unit tests

--- src/main/java/puzzleGame/PuzzleBoard.java
+++ src/main/java/puzzleGame/PuzzleBoard.java
@@ -4,67 +4,67 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PuzzleBoard {
 
-    private Map<Integer, PuzzlePiece> input;
+
+    public Map<Integer, PuzzlePiece> input;
 
     private PuzzlePiece[][] board;
 
     private String outputFileName = "puzzleResult.txt";
 
-    private void initBoard(){
+    private void initBoard() {
         int piecesAmount = input.size();
-        for(int i = 1; i <= piecesAmount/2; i++){
-            if(piecesAmount%i == 0){
-                board = new PuzzlePiece[i][piecesAmount/i];
-                if(solve()){
+        for (int i = 1; i <= piecesAmount / 2; i++) {
+            if (piecesAmount % i == 0) {
+                board = new PuzzlePiece[i][piecesAmount / i];
+                if (solve()) {
                     break;
                 }
             }
         }
     }
 
-    public boolean solve(){
+    public boolean solve() {
         return true;
     }
 
-    private boolean validateInput(){
+    private boolean validateInput() {
         StringBuilder errors = new StringBuilder();
         List<Integer> missingIds = validatePartsIds();
         return true;
     }
 
-    private List<Integer> validatePartsIds(){
+    private List<Integer> validatePartsIds() {
         List<Integer> missingIds = new ArrayList<Integer>();
-        for (int i = 1; i< input.size(); i++){
-            if(input.get(i) == null){
+        for (int i = 1; i < input.size(); i++) {
+            if (input.get(i) == null) {
                 missingIds.add(i);
             }
         }
         return missingIds;
     }
 
-    private List<Integer> validateIdsInRange(){
+    private List<Integer> validateIdsInRange() {
         List<Integer> notInRangeIds = new ArrayList<Integer>();
-        for (Map.Entry<Integer, PuzzlePiece> entry : input.entrySet())
-        {
+        for (Map.Entry<Integer, PuzzlePiece> entry : input.entrySet()) {
             int id = entry.getKey();
-            if(id < 0 || id > input.size()){
+            if (id < 0 || id > input.size()) {
                 notInRangeIds.add(id);
             }
         }
         return notInRangeIds;
     }
 
-    private List<Integer> validateFormat(){
+    private List<Integer> validateFormat() {
         List<Integer> invalidIds = new ArrayList<Integer>();
-        for (Map.Entry<Integer, PuzzlePiece> entry : input.entrySet())
-        {
+        for (Map.Entry<Integer, PuzzlePiece> entry : input.entrySet()) {
             PuzzlePiece piece = entry.getValue();
-            if(!piece.isValid()){
+            if (!piece.isValid()) {
                 invalidIds.add(entry.getKey());
             }
         }
@@ -75,5 +75,25 @@ public class PuzzleBoard {
         BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName, true));
         writer.append(content);
         writer.close();
+    }
+
+
+    // Added for TestPuzzleValidator unit tests
+    public PuzzleBoard(){
+        input = new HashMap<>();
+    }
+
+
+    // Added for TestPuzzleValidator unit tests
+    public void addPiece(int id, int left, int top, int right, int bottom) {
+
+        PuzzlePiece piece = new PuzzlePiece(left, top, right, bottom);
+        input.put(id, piece);
+
+    }
+
+    // Added for TestPuzzleValidator unit tests
+    public Map<Integer, PuzzlePiece> getInput() {
+        return input;
     }
 }
--- src/main/java/puzzleGame/PuzzleValidatorUtil.java
+++ src/main/java/puzzleGame/PuzzleValidatorUtil.java
@@ -1,10 +1,75 @@
 package puzzleGame;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PuzzleValidatorUtil {
 
-    public boolean isValidPuzzle(Map<Integer, PuzzlePiece> pieceList, String outputFilePath){
+    ArrayList<PuzzlePiece> solution = new ArrayList<>();
+    int rowNumber;
+    int columnNumber;
+
+
+    public boolean isValidPuzzle(Map<Integer, PuzzlePiece> pieceList, String outputFilePath) throws IOException {
+
+        int rowNumber = 0;
+        int columnNumber = 0;
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(outputFilePath)))) {
+
+            String puzzleRow;
+
+            while ((puzzleRow = br.readLine()) != null) {
+                rowNumber++;
+                String[] piecesLine = puzzleRow.split(" ");
+
+                for (columnNumber = 0; columnNumber < piecesLine.length; columnNumber++) {
+                    //Get piece id
+                    int pieceId = Integer.parseInt(piecesLine[columnNumber].trim());
+                    //Get piece shape from Map and add it to the puzzle solution
+                    solution.add(pieceList.get(pieceId));
+                }
+            }
+        }
+
+        this.rowNumber = rowNumber;
+        this.columnNumber = columnNumber;
+
+        return validateSolution();
+    }
+
+    private boolean validateSolution() {
+
+        for (int index = 0; index < solution.size(); index++) {
+
+            if (index >= 0 && index < columnNumber) { //First row, top should be 0
+                if (solution.get(index).getTop() != 0)
+                    return false;
+            }
+
+            if (index >= (columnNumber * (rowNumber - 1)) && index <= (columnNumber * rowNumber - 1)) //last row, bottom should be 0
+            {
+                if (solution.get(index).getBottom() != 0)
+                    return false;
+            }
+
+            if (index % columnNumber == 0) //First column, left should be 0
+            {
+                if (solution.get(index).getLeft() != 0)
+                    return false;
+            }
+
+            if (index != 0 && index % columnNumber == index) //Last column, right should be 0
+            {
+                if (solution.get(index).getRight() != 0)
+                    return false;
+            }
+        }
+
         return true;
     }
+
+
 }
