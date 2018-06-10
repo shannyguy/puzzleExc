01b3a981d6cd136906841ec2b0af913b194ba15b
Guy, Shanny (sg234e) <sg234e@intl.att.com>
2018-06-10 14:09:43 +0300

    Fixed according to Amir's comments.

--- src/main/java/puzzleGame/PuzzleErrors.java
+++ src/main/java/puzzleGame/PuzzleErrors.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PuzzleErrors {
-    public static String WRONG_NUMBER_OF_ELEMEMNTS_VALUE = "Illegal number of elements value in file: %s";
-    public static String MISSING_PUZZLE_ELEMENTS = "Missing puzzle element(s) with the following IDs: %s";
-    public static String WRONG_ELEMENT_IDS = "Puzzle of size <%s> cannot have the following IDs: %s";
-    public static String WRONG_ELEMENT_FORMAT = "Puzzle ID <%s> has wrong data: <%s>";
-    public static String WRONG_NUMBER_OF_STRAIGHT_EDGES = "Cannot solve puzzle: wrong number of straight edges";
-    public static String MISSING_CORNERS_ERROR = "Cannot solve puzzle: missing corner element: %s";
-    public static String SUM_OF_EDGES_NOT_ZERO = "Cannot solve puzzle: sum of edges is not zero";
-    public static String CANNOT_SOLVE_PUZZLE = "Cannot solve puzzle: it seems that there is no proper solution";
+    public String WRONG_NUMBER_OF_ELEMEMNTS_VALUE = "Illegal number of elements value in file: %s";
+    public String MISSING_PUZZLE_ELEMENTS = "Missing puzzle element(s) with the following IDs: %s";
+    public String WRONG_ELEMENT_IDS = "Puzzle of size <%s> cannot have the following IDs: %s";
+    public String WRONG_ELEMENT_FORMAT = "Puzzle ID <%s> has wrong data: <%s>";
+    public String WRONG_NUMBER_OF_STRAIGHT_EDGES = "Cannot solve puzzle: wrong number of straight edges";
+    public String MISSING_CORNERS_ERROR = "Cannot solve puzzle: missing corner element: %s";
+    public String SUM_OF_EDGES_NOT_ZERO = "Cannot solve puzzle: sum of edges is not zero";
+    public String CANNOT_SOLVE_PUZZLE = "Cannot solve puzzle: it seems that there is no proper solution";
 
     private List<String> errorsList = new ArrayList<String>();
 
